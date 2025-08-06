999f3c5ab9793d9ce275b35605120b69a116410b
zonesky-1 <r3zonery@gmail.com>
2025-08-06 21:38:42 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const session = require("telegraf/session");
+const {
+  handleBuy, handleProductSelected, handleWaitUpload,
+  handlePaymentProof, handleApprove, handleReject,
+  handleDeploy, handleStatus, handleHistory
+} = require("./handlers/buyHandler");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use(session());
+
+bot.command("buy", handleBuy);
+bot.command("status", handleStatus);
+bot.command("history", handleHistory);
+bot.action(/buy_(.+)/, ctx => handleProductSelected(ctx, ctx.match[1]));
+bot.action("wait_upload_bukti", handleWaitUpload);
+bot.on("photo", handlePaymentProof);
+bot.action(/approve_(\\d+)/, ctx => handleApprove(ctx, ctx.match[1]));
+bot.action(/reject_(\\d+)/, ctx => handleReject(ctx, ctx.match[1]));
+bot.on("text", handleDeploy);
+
+bot.launch().then(() => console.log("🤖 Bot aktif di Railway"));
