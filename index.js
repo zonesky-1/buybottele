8c4288e8ca79722916a5d53d2ec4d28c0cc3f3a1
zonesky-1 <r3zonery@gmail.com>
2025-08-06 22:11:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
-const { Telegraf } = require("telegraf");
-const session = require("telegraf/session");
+const { Telegraf, session } = require("telegraf");
 const {
-  handleBuy, handleProductSelected, handleWaitUpload,
-  handlePaymentProof, handleApprove, handleReject,
-  handleDeploy, handleStatus, handleHistory
+  handleBuy,
+  handleProductSelected,
+  handleWaitUpload,
+  handlePaymentProof,
+  handleApprove,
+  handleReject,
+  handleDeploy,
+  handleStatus,
+  handleHistory
 } = require("./handlers/buyHandler");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -16,8 +21,8 @@ bot.command("history", handleHistory);
 bot.action(/buy_(.+)/, ctx => handleProductSelected(ctx, ctx.match[1]));
 bot.action("wait_upload_bukti", handleWaitUpload);
 bot.on("photo", handlePaymentProof);
-bot.action(/approve_(\\d+)/, ctx => handleApprove(ctx, ctx.match[1]));
-bot.action(/reject_(\\d+)/, ctx => handleReject(ctx, ctx.match[1]));
+bot.action(/approve_(\d+)/, ctx => handleApprove(ctx, ctx.match[1]));
+bot.action(/reject_(\d+)/, ctx => handleReject(ctx, ctx.match[1]));
 bot.on("text", handleDeploy);
 
 bot.launch().then(() => console.log("🤖 Bot aktif di Railway"));
